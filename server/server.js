02c5ed8bfc447f2412b7da8f60aaa5c1cc489bea
Kee Boon Hwee <2717921K@student.gla.ac.uk>
2023-01-27 19:21:18 +0800

    moved frontend source codes into client folder

--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
-import express from 'express'
-import cors    from 'cors'
+const express = require('express')
+const cors    = require('cors')
 /*-------------------------------------------------------*/
 const port    = 5000
 const app     = express()
 /*-------------------------------------------------------*/
-app.use(cors())
-app.use(express.json())
+app.use( cors() )
+app.use( express.json() )
 app.listen(port, () => 
 {
     console.log(`Server listening on the port:${port}`);
 });
 /*-------------------------------------------------------*/
+app.get('/' , async(req,res)=>{
+    const { id , password } = req.body;
+    res.status(200).json({ 'message': `Server listening on the port:${port}`})
+})
+/*-------------------------------------------------------*/
 app.post('/login' , async(req,res)=>{
     const { id , password } = req.body;
     res.status(200).json({ 'id': id , 'password': password})
 })
+/*-------------------------------------------------------*/
