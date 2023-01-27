640108810aeeff550b6d1bf0e96a4fb49c841401
Kee Boon Hwee <2717921K@student.gla.ac.uk>
2023-01-28 02:52:53 +0800

    add simple page to test server connection

--- server/server.js
+++ server/server.js
@@ -1,23 +1,32 @@
-const express = require('express')
-const cors    = require('cors')
+const express    = require('express')
+const cors       = require('cors')
 /*-------------------------------------------------------*/
-const port    = 5000
-const app     = express()
+const port       = 5000
+const app        = express()
 /*-------------------------------------------------------*/
-app.use( cors() )
 app.use( express.json() )
+app.use( cors() )
+/*-------------------------------------------------------*/
 app.listen(port, () => 
 {
     console.log(`Server listening on the port:${port}`);
 });
 /*-------------------------------------------------------*/
 app.get('/' , async(req,res)=>{
-    const { id , password } = req.body;
     res.status(200).json({ 'message': `Server listening on the port:${port}`})
 })
 /*-------------------------------------------------------*/
 app.post('/login' , async(req,res)=>{
-    const { id , password } = req.body;
-    res.status(200).json({ 'id': id , 'password': password})
+    if(req.method == 'POST')
+    {
+        const { id , password } = req.body
+        console.log( '1', id, password )
+        res.status(200).json( {'id' : id , 'password' : password } )
+    }
+    else
+    {
+        res.status(404).json( {'error' : 'invalid request' } )
+    }
+
 })
 /*-------------------------------------------------------*/
