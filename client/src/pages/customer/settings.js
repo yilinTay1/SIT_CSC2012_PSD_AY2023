48f7bac30314956d6df8a17c60b94b37a2439c02
Kee Boon Hwee <2717921K@student.gla.ac.uk>
2023-03-16 19:16:55 +0800

    add dynamic customer settings

--- client/src/pages/customer/settings.js
+++ client/src/pages/customer/settings.js
@@ -4,25 +4,40 @@ import CustomerNavBar from "../../components/customer_view/navigation/navbar";
 import { Notifications } from "../../components/customer_view/settings/notification";
 import React, { useRef, useEffect, useState } from "react";
 import NextLink from "next/link";
+import { firebase_app , firebase_fs } from '../../components/firebase/firebase-config';
+import { collection , getDocs , doc , setDoc , addDoc, getDoc } from 'firebase/firestore';
 
 const Settings = () => {
   const runOnce = useRef(true);
   const [buyer, setBuyer] = useState(false);
+
+  const [firstName, setFirstName] = useState("");
+  const [lastname , setLastname ] = useState("");
+  const [email    , setEmail    ] = useState("");
+
   useEffect(() => {
     if (runOnce.current) {
       runOnce.current = false;
       const isbuyer = sessionStorage.getItem("buyer");
+      const uid = sessionStorage.getItem('uid');
+      console.log( "uid : " , uid )
       if (isbuyer) setBuyer(true);
+      if(uid)
+      {
+        const customer =  doc( firebase_fs , 'customers', uid )
+        getDoc(customer).then( response => response.data()    )
+                        .then( response =>{
+                          const { firstName , lastName , email  } = response
+                          setFirstName(firstName)
+                          setLastname(lastName)
+                          setEmail(email)
+                        })
+                        .catch( err =>  {} )
+      }
       console.log("/");
     }
   }, [runOnce]);
 
-  //   useEffect(() => {
-  //     if (runOnce.current) {
-  //       runOnce.current = false;
-  //       console.log("Settings");
-  //     }
-  //   }, [runOnce]);
 
   return (
     <>
@@ -45,15 +60,15 @@ const Settings = () => {
             <Box sx={{ pt: 3 }}>
               <Grid container spacing={2}>
                 <Typography color="textPrimary" variant="h3" style={{ paddingRight: 20 }}>
-                  First Name
+                 {firstName}
                 </Typography>
                 <Typography color="textPrimary" variant="h3">
-                  Last Name
+                  {lastname}
                 </Typography>
               </Grid>
               <br></br>
               <Typography color="textPrimary" variant="h5">
-                Email Address
+                {email}
               </Typography>
               <br></br>
               <NextLink href="/customer/change_pwd" passHref>
