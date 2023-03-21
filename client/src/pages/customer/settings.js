19f4bb921f29c3e6a13a823362f20eb94bd8d37b
chanxueli <2717950C@student.gla.ac.uk>
2023-03-21 14:57:29 +0800

    ammended customer settings styles

--- client/src/pages/customer/settings.js
+++ client/src/pages/customer/settings.js
@@ -4,40 +4,39 @@ import CustomerNavBar from "../../components/customer_view/navigation/navbar";
 import { Notifications } from "../../components/customer_view/settings/notification";
 import React, { useRef, useEffect, useState } from "react";
 import NextLink from "next/link";
-import { firebase_app , firebase_fs } from '../../components/firebase/firebase-config';
-import { collection , getDocs , doc , setDoc , addDoc, getDoc } from 'firebase/firestore';
+import { firebase_app, firebase_fs } from "../../components/firebase/firebase-config";
+import { collection, getDocs, doc, setDoc, addDoc, getDoc } from "firebase/firestore";
 
 const Settings = () => {
   const runOnce = useRef(true);
   const [buyer, setBuyer] = useState(false);
 
   const [firstName, setFirstName] = useState("");
-  const [lastname , setLastname ] = useState("");
-  const [email    , setEmail    ] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     if (runOnce.current) {
       runOnce.current = false;
       const isbuyer = sessionStorage.getItem("buyer");
-      const uid = sessionStorage.getItem('uid');
-      console.log( "uid : " , uid )
+      const uid = sessionStorage.getItem("uid");
+      console.log("uid : ", uid);
       if (isbuyer) setBuyer(true);
-      if(uid)
-      {
-        const customer =  doc( firebase_fs , 'customers', uid )
-        getDoc(customer).then( response => response.data()    )
-                        .then( response =>{
-                          const { firstName , lastName , email  } = response
-                          setFirstName(firstName)
-                          setLastname(lastName)
-                          setEmail(email)
-                        })
-                        .catch( err =>  {} )
+      if (uid) {
+        const customer = doc(firebase_fs, "customers", uid);
+        getDoc(customer)
+          .then((response) => response.data())
+          .then((response) => {
+            const { firstName, lastName, email } = response;
+            setFirstName(firstName);
+            setLastname(lastName);
+            setEmail(email);
+          })
+          .catch((err) => {});
       }
       console.log("/");
     }
   }, [runOnce]);
-
 
   return (
     <>
@@ -58,20 +57,24 @@ const Settings = () => {
         >
           <Container maxWidth="lg">
             <Box sx={{ pt: 3 }}>
-              <Grid container spacing={2}>
-                <Typography color="textPrimary" variant="h3" style={{ paddingRight: 20 }}>
-                 {firstName}
+              <div style={{ display: "flex", textAlign: "center" }}>
+                <Typography color="textPrimary" variant="h3" style={{ paddingRight: 10 }}>
+                  {firstName}
                 </Typography>
                 <Typography color="textPrimary" variant="h3">
                   {lastname}
                 </Typography>
-              </Grid>
+              </div>
               <br></br>
               <Typography color="textPrimary" variant="h5">
                 {email}
               </Typography>
               <br></br>
-              <NextLink href="/customer/change_pwd" passHref style={{ cursor: "pointer", color: "black", textDecoration: "None" }}>
+              <NextLink
+                href="/customer/change_pwd"
+                passHref
+                style={{ cursor: "pointer", color: "black", textDecoration: "None" }}
+              >
                 <Button color="primary" variant="contained">
                   Change Password
                 </Button>
